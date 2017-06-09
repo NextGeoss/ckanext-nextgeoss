942c7719519cca6b2a772984de7162382371b13d
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2017-06-09 16:20:26 +0200

    Tweak UI for dataset items

---
+++ ckanext/nextgeoss/helpers.py
@@ -0,0 +1,10 @@
+from ckan.plugins.toolkit import get_action
+
+
+def get_org_title(id):
+    try:
+        org = get_action('organization_show')({}, {'id': id})
+
+        return org.get('title', '')
+    except Exception:
+        return ''
--- ckanext/nextgeoss/plugin.py
+++ ckanext/nextgeoss/plugin.py
@@ -1,11 +1,14 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+from ckanext.nextgeoss import helpers
+
 
 class NextgeossPlugin(plugins.SingletonPlugin):
     ''' Plugin for the NextGEOSS theme.
     '''
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
     # IConfigurer
 
@@ -13,3 +16,10 @@ class NextgeossPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'nextgeoss')
+
+    # ITemplateHelpers
+
+    def get_helpers(self):
+        return {
+            'nextgeoss_get_org_title': helpers.get_org_title
+        }
