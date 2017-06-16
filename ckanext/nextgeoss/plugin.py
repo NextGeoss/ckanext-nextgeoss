6e0401cf7b247f6174b971ed491ea6d1d76bbbd1
kmbn <github@nevermindtheumlauts.com>
2017-06-16 09:41:26 +0200

    Rename Organizations to Providers
    Some links are still generated using /organization/, but they
    are rerouted to the correct /provider/ URL and are only visible
    to users in tooltips and browser status bars on mouseover.

--- ckanext/nextgeoss/plugin.py
+++ ckanext/nextgeoss/plugin.py
@@ -30,6 +30,43 @@ class NextgeossPlugin(plugins.SingletonPlugin):
     # IRoutes
 
     def before_map(self, map):
+        # Rename organizations
+        map.redirect('/organization', '/provider',
+                     _redirect_code='301 Moved Permanently')
+        map.redirect('/organization/{url:.*}', '/provider/{url}',
+                     _redirect_code='301 Moved Permanently')
+        org_controller = 'ckan.controllers.organization:OrganizationController'
+        with routes.mapper.SubMapper(map, controller=org_controller) as m:
+            m.connect('providers_index', '/provider', action='index')
+            m.connect('/provider/list', action='list')
+            m.connect('/provider/new', action='new')
+            m.connect('/provider/{action}/{id}',
+                      requirements=dict(action='|'.join([
+                          'delete',
+                          'admins',
+                          'member_new',
+                          'member_delete',
+                          'history'
+                          'followers',
+                          'follow',
+                          'unfollow',
+                      ])))
+            m.connect('provider_activity', '/provider/activity/{id}',
+                      action='activity', ckan_icon='time')
+            m.connect('provider_read', '/provider/{id}', action='read')
+            m.connect('provider_about', '/provider/about/{id}',
+                      action='about', ckan_icon='info-sign')
+            m.connect('provider_read', '/provider/{id}', action='read',
+                      ckan_icon='sitemap')
+            m.connect('provider_edit', '/provider/edit/{id}',
+                      action='edit', ckan_icon='edit')
+            m.connect('provider_members', '/provider/edit_members/{id}',
+                      action='members', ckan_icon='group')
+            m.connect('provider_bulk_process',
+                      '/provider/bulk_process/{id}',
+                      action='bulk_process', ckan_icon='sitemap')
+
+        # Add static pages
         controller = 'ckanext.nextgeoss.controller:StaticController'
         with routes.mapper.SubMapper(map, controller=controller) as m:
             m.connect('privacy', '/privacy', action='privacy')
