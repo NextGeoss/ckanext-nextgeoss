5b4465660449ac9792a708bae89a31c1144de3c5
kmbn <github@nevermindtheumlauts.com>
2017-11-30 16:48:56 +0100

    Speed up page rendering

--- ckanext/nextgeoss/helpers.py
+++ ckanext/nextgeoss/helpers.py
@@ -1,21 +1,6 @@
 from ckan.plugins.toolkit import get_action
 from ckan.common import config
 
-def get_org_title(id):
-    try:
-        org = get_action('organization_show')({}, {'id': id})
-
-        return org.get('title', '')
-    except Exception:
-        return ''
-
-def get_org_logo(id):
-    try:
-        org = get_action('organization_show')({}, {'id': id})
-
-        return org.get('image_display_url', '')
-    except Exception:
-        return ''
 
 def get_jira_script():
     jira_script = config.get('ckanext.nextgeoss.jira_issue_tracker')
