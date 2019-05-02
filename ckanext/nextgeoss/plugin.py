2784cba336cad9ce5d5762ac2751c367c29cde0e
Georgiana Bere <bere.georgi@gmail.com>
2019-05-02 16:18:54 +0200

    Format content in OpenSearch page into panels

--- ckanext/nextgeoss/plugin.py
+++ ckanext/nextgeoss/plugin.py
@@ -29,7 +29,7 @@ class NextgeossPlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'nextgeoss')
+        toolkit.add_resource('fanstatic', 'info-panel')
 
     # ITemplateHelpers
 
